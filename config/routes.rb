c3bae049ceee370dbaec0a1ae29bdd9ad8da633b
tabun_muri <tabun.muri100@gmail.com>
2015-07-02 15:53:18 +0900

    routesで、/usersを呼んでいるのに、/api/users/が呼ばれてしまっていた問題を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  
+  resources :users
+
   namespace :api do
     resources :users
   end
-  resources :users
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
