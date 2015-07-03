83170092f794279d1cc5e95f14db3dabc38ccfd7
tabun_muri <tabun,muri100@gmail.com>
2015-07-03 17:41:47 +0900

    validationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+  validates :password,
+            :presence => true,
+            :length => { :minimum => 8 }
+
+  # メールアドレスフォーマットの検証（完璧な正規表現ではない）
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            :presence => true,
+            :format => { :with => VALID_EMAIL_REGEX }
+
+  validates :name,
+            :presence => true,
+            :length => { :maximum => 120 }
+
 end
